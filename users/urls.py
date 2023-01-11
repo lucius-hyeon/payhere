2f722fc729618eb3d38ed9b41072f0709696c361
lucius-hyeon <lucius.hyeon@gmail.com>
2023-01-11 17:09:56 +0900

    Feature: 로그아웃(blacklist) 기능 추가

--- users/urls.py
+++ users/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
+    TokenBlacklistView,
 )
 from .views import CustomTokenObtainPairView
 
 urlpatterns = [
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
 ]
